7648535595d98f67413e83d7d802e6954f79701e
Louis-Philippe Lemieux Perreault <louis-philippe.lemieux.perreault@statgen.org>
2016-07-07 13:56:30 -0400

    Added functions to add variants

--- sql/create.sql
+++ sql/create.sql
@@ -79,4 +79,73 @@ CREATE TABLE IF NOT EXISTS gwas_results.Results (
     CONSTRAINT fk_gr_variant FOREIGN KEY (variant_id) REFERENCES gwas_results.Variant (id)
 );
 
+
+-- Indexing
+CREATE INDEX ON gwas_results.Variant(chrom, position, reference);
+
+
+-- Functions
+CREATE OR REPLACE FUNCTION gwas_results.InsertVariant(
+    var_chrom varchar(2),
+    var_pos integer,
+    var_ref varchar(100),
+    var_alt varchar(100),
+    array_name varchar(50)
+) RETURNS VOID AS
+$$
+DECLARE
+    rec RECORD;
+    last_variant integer;
+BEGIN
+    SELECT id, alt INTO STRICT rec FROM gwas_results.Variant WHERE chrom=var_chrom AND position=var_pos AND reference=var_ref;
+
+    -- Is the alternative allele is in the array?
+    IF NOT var_alt = ANY(rec.alt)
+    THEN
+        UPDATE gwas_results.Variant SET alt = rec.alt || var_alt WHERE id=rec.id;
+    END IF;
+
+    -- Is the variant in the many2many table for this array?
+    IF NOT array_name = ANY(SELECT genotyping_array FROM gwas_results.ArrayVariants WHERE variant=rec.id)
+    THEN
+        INSERT INTO gwas_results.ArrayVariants (variant, genotyping_array)
+            VALUES (rec.id, array_name);
+    END IF;
+
+    EXCEPTION
+        WHEN NO_DATA_FOUND THEN
+            -- The variant doesn't exist, so we add it
+            INSERT INTO gwas_results.Variant (chrom, position, reference, alt[1])
+                VALUES (var_chrom, var_pos, var_ref, var_alt)
+                RETURNING id INTO last_variant;
+            -- Inserting into the many2many table
+            INSERT INTO gwas_results.ArrayVariants (variant, genotyping_array)
+                VALUES (last_variant, array_name);
+
+        WHEN TOO_MANY_ROWS THEN
+            -- This is impossible...
+            RAISE EXCEPTION 'Multiple entries for variant chr%:%/%', var_chrom, var_pos, var_ref;
+END;
+$$
+LANGUAGE plpgsql STRICT;
+
+
+CREATE OR REPLACE FUNCTION gwas_results.InsertVariantArray(
+    var_chrom varchar(2)[],
+    var_pos integer[],
+    var_ref varchar(100)[],
+    var_alt varchar(100)[],
+    array_name varchar(50)
+) RETURNS VOID AS
+$$
+BEGIN
+    FOR i IN 1 .. array_upper(var_chrom, 1)
+    LOOP
+        PERFORM gwas_results.InsertVariant(var_chrom[i], var_pos[i], var_ref[i], var_alt[i], array_name);
+    END LOOP;
+END;
+$$
+LANGUAGE plpgsql STRICT;
+
+
 COMMIT;
