fcd00b91fc8b4cca360f1dfabdac32959af0258e
Louis-Philippe Lemieux Perreault <louis-philippe.lemieux.perreault@statgen.org>
2016-07-08 10:44:08 -0400

    Now uses options for DB parameters and can now import a study

--- python/import.py
+++ python/import.py
@@ -3,14 +3,13 @@
 
 from __future__ import print_function
 
+import sys
 import logging
 import argparse
 from getpass import getpass
 
 import pyfaidx
 import psycopg2
-
-from settings import DATABASE as DBINFO
 
 
 # Logging configuration
@@ -30,16 +29,51 @@ def main():
     """The main."""
     args = parse_args()
 
-    # Reading the reference
-    human_ref = pyfaidx.Fasta(args.reference)
-
     # Creating the connection and getting the metadata
-    conn, cur = db_connect()
+    conn, cur = db_connect(args.host_name, args.database_name, args.user_name)
 
     # If array
     if args.command == "array":
+        # Reading the reference
+        human_ref = pyfaidx.Fasta(args.reference)
+
         import_array_name(conn, cur, args)
         import_array_variants(conn, cur, human_ref, args)
+
+    # If stydy
+    if args.command == "study":
+        import_study(conn, cur, args)
+
+
+def import_study(db_conn, db_cur, args):
+    """Adds a study to the DB."""
+    try:
+        db_cur.execute(
+            "INSERT INTO gwas_results.Study (name, parent, genotyping_array) "
+            "    VALUES (%s, %s, %s)",
+            (args.name, args.parent_study, args.array),
+        )
+
+    except psycopg2.IntegrityError as exception:
+        if "duplicate key value violates unique constraint" in str(exception):
+            if "Key (name)=" in str(exception):
+                logger.error("Study '{}' already exists".format(args.name))
+                sys.exit(1)
+
+        elif "violates foreign key constraint" in str(exception):
+            if "Key (genotyping_array)=" in str(exception):
+                logger.error("Genotyping array '{}' doesn't "
+                             "exists".format(args.array))
+                sys.exit(1)
+
+            elif "Key (parent)=" in str(exception):
+                logger.error("Parent study '{}' doesn't "
+                             "exists".format(args.parent_study))
+                sys.exit(1)
+
+        raise
+
+    db_conn.commit()
 
 
 def import_array_name(db_conn, db_cur, args):
@@ -171,15 +205,25 @@ def encode_chromosome(chrom):
     return chrom.upper()
 
 
-def db_connect():
-    """Performs database connection using a database settings."""
+def db_connect(hostname, database, username):
+    """Performs database connection using a database settings.
+
+    Args:
+        hostname (str): The host name of the database server.
+        database (str): The name of the database.
+        username (str): The name of the user.
+
+    """
     # Asking for the password
-    pw = getpass("Password for {}: ".format(DBINFO["username"]))
+    pw = getpass("Password for {}: ".format(username))
 
     # The connection
     conn = psycopg2.connect(
         "dbname='{database}' user='{username}' host='{hostname}' "
-        "password='{password}'".format(password=pw, **DBINFO)
+        "password='{password}'".format(
+            database=database, username=username, hostname=hostname,
+            password=pw,
+        )
     )
     cur = conn.cursor()
 
@@ -192,9 +236,24 @@ def parse_args():
 
     parent_parser = argparse.ArgumentParser(add_help=False)
 
+    # The database parameters
+    group = parent_parser.add_argument_group("Database options")
+    group.add_argument(
+        "-H", "--host-name", type=str, metavar="HOST", required=True,
+        help="The host name of the database server.",
+    )
+    group.add_argument(
+        "-U", "--user-name", type=str, metavar="USER", required=True,
+        help="The user name for the database.",
+    )
+    group.add_argument(
+        "-D", "--database-name", type=str, metavar="NAME", required=True,
+        help="The name of the database.",
+    )
+
     subparser = parser.add_subparsers(
         dest="command",
-        help=("What kind of information to import")
+        help="What kind of information to import",
     )
     subparser.required = True
 
@@ -205,13 +264,13 @@ def parse_args():
         parents=[parent_parser]
     )
 
-    group = import_array_parser.add_argument_group("Genotyping array option")
+    group = import_array_parser.add_argument_group("Genotyping array options")
     group.add_argument(
         "--name", type=str, metavar="NAME", required=True,
         help="The name of the array.",
     )
 
-    group = import_array_parser.add_argument_group("Annotation file")
+    group = import_array_parser.add_argument_group("Annotation file options")
     group.add_argument(
         "--annotation", type=str, metavar="FILE", required=True,
         help="The annotation provided by the chip.",
@@ -233,10 +292,31 @@ def parse_args():
         help="The field containing the genotype. [%(default)s]",
     )
 
-    group = import_array_parser.add_argument_group("Reference")
+    group = import_array_parser.add_argument_group("Reference options")
     group.add_argument(
         "--reference", type=str, metavar="FILE", required=True,
         help="The human reference fasta file.",
+    )
+
+    # The chip import
+    import_study_parser = subparser.add_parser(
+        "study",
+        help="Import a study.",
+        parents=[parent_parser]
+    )
+
+    group = import_study_parser.add_argument_group("Study options")
+    group.add_argument(
+        "--name", type=str, metavar="STUDY", required=True,
+        help="The name of the study.",
+    )
+    group.add_argument(
+        "--array", type=str, metavar="ARRAY", required=True,
+        help="The name of the genotyping arry.",
+    )
+    group.add_argument(
+        "--parent-study", type=str, metavar="STUDY",
+        help="The name of the parent study (if any).",
     )
 
     return parser.parse_args()
